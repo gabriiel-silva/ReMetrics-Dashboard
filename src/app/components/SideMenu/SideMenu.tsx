67634fafb6d3c92bd4c43f0b7456bb687fb089f8
Gabriel <gabrielsouzasilva2004@gmail.com>
2024-11-22 17:11:02 -0300

    routes staged

---
+++ src/app/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SideMenu = () => {
+    return (
+        <ul>
+            <li>Profile</li>
+            <li>Analytics</li>
+            <li>Settings</li>
+        </ul>
+    )
+}
+export default SideMenu;
---
+++ src/app/pages/dashboard/analitycs/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Analitycs = () => {
+    return (
+        <h1>Analitycs</h1>
+    )
+}
+export default Analitycs;
